3b75654c420a6b3913603db753d055cde1f87a7e
jesuanp <jespaterm@gmail.com>
2023-11-25 17:10:43 -0400

    Agregando .env en el proyecto

--- src/db.js
+++ src/db.js
@@ -1,8 +1,13 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config()
+
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
 
 const accountModel = require('./models/account/modelAccount.js');
 
-const sequelize = new Sequelize('postgres://postgres:password@localhost:5432/registrocuentas', {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/registrocuentas`, {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
