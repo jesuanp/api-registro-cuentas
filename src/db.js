1b462174eb2a0b861e7dbf768193331e65fa4430
jesuanp <jespaterm@gmail.com>
2023-11-25 22:49:21 -0400

    Agregando una variable de entorno

--- src/db.js
+++ src/db.js
@@ -4,10 +4,11 @@ require('dotenv').config()
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
+const DATABASE = process.env.DATABASE || 'registrocuentas';
 
 const accountModel = require('./models/account/modelAccount.js');
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/registrocuentas`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DATABASE}`, {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
